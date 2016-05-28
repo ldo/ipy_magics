4b216daf88776d19d9786d9324ac52215eb7ad33
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-05-28 07:18:12 +0000

    Replace pspng with psgraf, with options for setting pixel density and paper size

--- ps_magics.py
+++ ps_magics.py
@@ -9,29 +9,47 @@ from IPython.display import \
     display_png
 from IPython.core import \
     magic
+import IPython.core.magic_arguments as \
+    magicargs
 
 @magic.magics_class
 class PSMagics(magic.Magics) :
     "defines cell magics for executing PostScript code using Ghostscript and displaying" \
     " the output:\n" \
     " %%pstext -- displays returned output as text.\n" \
-    " %%pspng -- displays returned output as a PNG graphic stream."
+    " %%psgraf -- displays returned output as a graphic."
 
     @staticmethod
-    def run_gs(input, binary) :
+    def run_gs(input, binary, pixel_density = None, papersize = None) :
       # internal routine handling common part of Ghostscript invocation.
+        args = \
+          (
+            "gs", "-q", "-dBATCH", "-dNOPAUSE",
+              # -dBATCH needed to turn off prompt (doc says -dNOPAUSE does this, but it
+              # lies).
+            "-sDEVICE=png16m", "-sOutputFile=/dev/stdout",
+          )
+        if pixel_density != None :
+            args += \
+                (
+                        "-r%(pixel_density)ux%(pixel_density)u"
+                    %
+                        {"pixel_density" : int(pixel_density)},
+                )
+        #end if
+        if papersize != None :
+            args += ("-sPAPERSIZE=%s" % papersize,)
+        #end if
         proc_gs = subprocess.Popen \
           (
             args =
-              (
-                "gs", "-q", "-dBATCH", "-dNOPAUSE",
-                  # -dBATCH needed to turn off prompt (doc says -dNOPAUSE does this, but it
-                  # lies).
-                "-sDEVICE=png16m", "-sOutputFile=/dev/stdout",
-                "/dev/stdin",
-                  # Side effect of -dBATCH is that gs no longer automatically reads from
-                  # stdin, need to specify explicit input filenames.
-              ),
+                    args
+                +
+                    (
+                        "/dev/stdin",
+                          # Side effect of -dBATCH is that gs no longer automatically reads from
+                          # stdin, need to specify explicit input filenames.
+                    ),
             stdin = subprocess.PIPE,
             stdout = subprocess.PIPE,
             #stderr = subprocess.PIPE, # Ghostscript returns traceback in stdout
@@ -66,10 +84,25 @@ class PSMagics(magic.Magics) :
     #end pstext
 
     @magic.cell_magic
-    def pspng(self, line, cell) :
-        "executes the cell contents as PostScript, and displays returned PNG graphic."
-        display_png(self.run_gs(cell.encode(), True), raw = True)
-    #end pspng
+    @magicargs.magic_arguments()
+    @magicargs.argument("-r", "--resolution", help = "output dpi, default = 72")
+    @magicargs.argument("--papersize", help = "paper size, e.g. a4")
+      # see /usr/share/ghostscript/9.19/Resource/Init/gs_statd.ps
+    def psgraf(self, line, cell) :
+        "executes the cell contents as PostScript, and displays the returned graphic."
+        args = magicargs.parse_argstring(PSMagics.psgraf, line)
+        display_png \
+          (
+            self.run_gs
+              (
+                input = cell.encode(),
+                binary = True,
+                pixel_density = getattr(args, "resolution", None),
+                papersize = getattr(args, "papersize", None),
+              ),
+            raw = True
+          )
+    #end psgraf
 
 #end PSMagics
 
